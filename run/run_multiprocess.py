4491f3087ec83ad8c02f988c0093a5e6d77a117a
Bryan Dedeurwaerder <23252092+BryanDedeur@users.noreply.github.com>
2023-03-08 13:39:04 -0800

    Update run

--- run/run_multiprocess.py
+++ run/run_multiprocess.py
@@ -11,14 +11,10 @@ if __name__ == '__main__':
     processes = []
 
     instances = []
-    instances = [
-                '../benchmarks/gdb/gdb2.dat',
-                '../benchmarks/gdb/gdb1.dat'
-                ]
     instance_path = "../bridge-graph-instances/standard/howe/"
     # loop through all the files in the directory and print out the file names
-    # for file_name in os.listdir(instance_path):
-    #     instances.append(instance_path+file_name)
+    for file_name in os.listdir(instance_path):
+        instances.append(instance_path+file_name)
 
     # seeds
     seeds = "8115,3520,8647,9420,3116,6377,6207,4187,3641,8591,3580,8524,2650,2811,9963,7537,3472,3714,8158,7284,6948,6119,5253,5134,7350,2652,9968,3914,6899,4715"
