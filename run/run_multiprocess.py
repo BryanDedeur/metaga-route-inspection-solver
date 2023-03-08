65aae68d7fe8c54246486d9a9af644b16dcad016
Bryan Dedeurwaerder <23252092+BryanDedeur@users.noreply.github.com>
2023-03-08 15:06:55 -0800

    Add improved recursive running

--- run/run_multiprocess.py
+++ run/run_multiprocess.py
@@ -6,24 +6,50 @@ def run_exe(arguments):
     print(cmd)
     os.system(cmd)
 
+def find_files_with_extension(dir, extensions):
+    """
+    Recursively find all files in root_dir and its subdirectories
+    that have one of the extensions in the extensions list.
+    
+    Args:
+    - root_dir: string, the root directory to start searching in.
+    - extensions: list of strings, the extensions to filter files by.
+    
+    Returns:
+    - list of strings, the absolute paths of all matching files.
+    """
+    # Initialize an empty list to hold the file paths.
+    file_paths = []
+    
+    # Loop over all files and directories in root_dir.
+    for entry in os.scandir(dir):
+        # If the entry is a directory, recursively call this function
+        # with the directory as the new root_dir.
+        if entry.is_dir():
+            file_paths.extend(find_files_with_extension(entry.path, extensions))
+        # If the entry is a file and its extension matches one of the
+        # extensions in the extensions list, add its path to the file_paths list.
+        elif entry.is_file() and os.path.splitext(entry.name)[1] in extensions:
+            file_paths.append(entry.path)
+    
+    # Return the list of matching file paths.
+    return file_paths
+
 if __name__ == '__main__':
     num_cores = multiprocessing.cpu_count()
     processes = []
 
-    instances = []
-    instance_path = "../bridge-graph-instances/standard/howe/"
-    # loop through all the files in the directory and print out the file names
-    for file_name in os.listdir(instance_path):
-        instances.append(instance_path+file_name)
+    extensions = ('.dat', '.csv')
+    instances = find_files_with_extension("benchmark_subset", extensions)
 
     # seeds
     seeds = "8115,3520,8647,9420,3116,6377,6207,4187,3641,8591,3580,8524,2650,2811,9963,7537,3472,3714,8158,7284,6948,6119,5253,5134,7350,2652,9968,3914,6899,4715"
 
     # depot configurations / k-values
-    k_depots = [[0,0]] # [[0,0],[0,0,0,0],[0,0,0,0,0,0,0,0]]
+    k_depots = [[0,0],[0,0,0,0],[0,0,0,0,0,0,0,0]]
 
     # heuristics
-    heuristics = ['MMMR', 'RR']
+    heuristics = ['MMMR']
 
     print("Running " + str(len(instances) * len(k_depots) * len(heuristics)) + " different configurations.")
 
